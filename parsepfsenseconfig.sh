661db30d0c7eed9968ed82c64a23a4f6a6661d6f
Julian <julianrseidel@gmail.com>
2017-01-09 12:09:36 -0800

    Update Added Username and Password Variables

--- parsepfsenseconfig.sh
+++ parsepfsenseconfig.sh
@@ -3,8 +3,11 @@
 # copies it with the date attached to the name, deletes ![CDATA[ and the ending ] from the usernames, connects to mysql, and 
 # runs commands listed in mac_name.txt
 
+username=""     # input username
+password=""     # input password
+
 cd /home/mysqlv10 			# change directory to home directory
 scp -r -P 1256 backup@192.168.1.1:/cf/conf/config.xml /home/mysqlv10/captiveportalbackup/ 			# copy config from server to local computer  
 cp /home/mysqlv10/captiveportalbackup/config.xml "/home/mysqlv10/captiveportalbackup/config.$(date +%F_%R)"			# copy config with date attached
 sed -i~ -f replacement.sed /home/mysqlv10/captiveportalbackup/config.xml			# remove ![CDATA[ and ending ]
-mysql -u radius -pradpass bandwidth < '/home/mysqlv10/mac_name.txt'			# connect to mysql and run commands in mac_name.txt
+mysql -u $username -$password bandwidth < '/home/mysqlv10/mac_name.txt'			# connect to mysql and run commands in mac_name.txt
